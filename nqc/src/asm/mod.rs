0769989ec7ed4b010bca4903d358193c2f5c7188
David Young <david@thedavidyoung.co.uk>
2025-01-14 21:26:29 +0000

    todo list

--- nqc/src/asm/mod.rs
+++ nqc/src/asm/mod.rs
@@ -1,2 +1,9 @@
 pub mod ast;
 pub mod parser;
+
+// 1. lexer to take source asm file into Vec<LabelOrOpcode>
+//    - parse hex and decimal numbers
+// 2. parser to convert string opcode names into proper opcode types
+//    and validate they they have the correct arguments
+// 3. assembler to convert into bytecode and resolve labels & jumps
+// 4. something to write out the binary file
