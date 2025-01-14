021cce56fb8d94dac4bf15d4b9f8ec5a6ad9cdd0
David Young <david@thedavidyoung.co.uk>
2025-01-14 21:23:20 +0000

    Handwritten lexer for asm

--- nqc/src/asm/parser.rs
+++ nqc/src/asm/parser.rs
@@ -1,10 +1,23 @@
-use lalrpop_util::lalrpop_mod;
+use crate::asm::ast::LabelOrOpcode;
 
-lalrpop_mod!(
-    #[allow(clippy::ptr_arg)]
-    #[rustfmt::skip]
-	asm
-);
+//TODO move into lexer module
+pub fn parse(input: &str) -> Vec<LabelOrOpcode> {
+    let mut out = Vec::new();
+
+    // TODO regexes etc.
+    for line in input.lines() {
+        if let Some(label) = line.strip_suffix(':') {
+            out.push(LabelOrOpcode::Label(label));
+        } else {
+            let mut iter = line.split_whitespace();
+            let opcode = iter.next().unwrap();
+            let args = iter.map(|arg| arg.parse().unwrap()).collect();
+            out.push(LabelOrOpcode::Opcode(opcode, args));
+        }
+    }
+
+    out
+}
 
 #[cfg(test)]
 mod test {
@@ -15,12 +28,26 @@ mod test {
     fn label_or_opcode() {
         for (case, expected) in [
             ("label:", vec![LabelOrOpcode::Label("label")]),
-            ("opcode 5\n", vec![LabelOrOpcode::Opcode("opcode", vec![])]),
+            ("opcode 5", vec![LabelOrOpcode::Opcode("opcode", vec![5])]),
+            (
+                "opcode 5 6 7",
+                vec![LabelOrOpcode::Opcode("opcode", vec![5, 6, 7])],
+            ),
+            (
+                "opcode 255",
+                vec![LabelOrOpcode::Opcode("opcode", vec![0xff])],
+            ),
         ] {
-            assert_eq!(
-                asm::LabelOrOpcodesParser::new().parse(case).unwrap(),
-                expected
-            );
+            dbg!(case);
+            assert_eq!(parse(case), expected,);
         }
     }
+
+    // #[test]
+    // fn opcode() {
+    //     assert_eq!(
+    //         asm::OpcodeParser::new().parse("game 3").unwrap(),
+    //         LabelOrOpcode::Opcode("game", vec![5])
+    //     );
+    // }
 }
